10b24ea6be9e79778c9b49b3c6364bad4a5b680a
Sakshi Soni <65129236+Sakshi-2100@users.noreply.github.com>
2023-06-03 13:51:01 +0530

    Final change

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Junior Year Undergrad", "Developer"],
+        strings: ["Btech CSE Graduate", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
